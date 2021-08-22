e4fcb99d1623e8392e5c5c03571913ad440505dc
LautaAndreani <lautaroandreani2016@gmail.com>
2021-08-22 10:28:17 -0300

    background

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", () => {
   images("mountain");
+  loadContainer.style.visibility = "hidden";
+  loadContainer.style.opacity = "0";
 });
 
 const container = document.querySelector(".gallery__container");
@@ -30,7 +32,7 @@ btn.addEventListener("click", () => {
   const input = document.getElementById("input__text").value;
   const logText = input.charAt(0).toUpperCase() + input.slice(1);
   if (input === "") {
-    notification("Oops... please complete the forms", "error");
+    notification("Oops... Please complete all fields", "error");
     // alert("Please complete the forms");
     return;
   } else {
@@ -73,6 +75,14 @@ function notification(message, status) {
   }, 1500);
   noti.classList.add("alert");
 }
+
+// Animation load
+const loadContainer = document.createElement("div");
+loadContainer.className = "load";
+loadContainer.innerHTML = `
+  <span><svg xmlns="http://www.w3.org/2000/svg" class="maximize" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#f7f9fc" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg></span>
+  `;
+document.body.appendChild(loadContainer);
 
 // Scroll Reveal
 const sr = ScrollReveal({
